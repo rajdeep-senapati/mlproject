41ae740a4e3e256af709b8ee2848ea19eb0e001f
rajdeep-senapati <rajdeepsenapati26@gmail.com>
2025-02-21 21:59:14 +0530

    Model Trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -55,20 +55,11 @@ class ModelTrainer:
                         "absolute_error",
                         "poisson",
                     ],
-                    # 'splitter':['best','random'],
-                    # 'max_features':['sqrt','log2'],
                 },
-                "Random Forest": {
-                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    # 'max_features':['sqrt','log2',None],
-                    "n_estimators": [8, 16, 32, 64, 128, 256]
-                },
+                "Random Forest": {"n_estimators": [8, 16, 32, 64, 128, 256]},
                 "Gradient Boosting": {
-                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
                     "learning_rate": [0.1, 0.01, 0.05, 0.001],
                     "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
-                    # 'criterion':['squared_error', 'friedman_mse'],
-                    # 'max_features':['auto','sqrt','log2'],
                     "n_estimators": [8, 16, 32, 64, 128, 256],
                 },
                 "Linear Regression": {},
@@ -83,7 +74,6 @@ class ModelTrainer:
                 },
                 "AdaBoost Regressor": {
                     "learning_rate": [0.1, 0.01, 0.5, 0.001],
-                    # 'loss':['linear','square','exponential'],
                     "n_estimators": [8, 16, 32, 64, 128, 256],
                 },
             }
